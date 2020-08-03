11b4e894ca2e0fc5a028b55a09cd0f949623f853
Stephen Richer <sr467@bath.ac.uk>
2020-08-03 23:55:29 +0100

    Add read_samples function and remove others.

--- snake_setup.py
+++ snake_setup.py
@@ -71,114 +71,13 @@ def load_samples(samples_file):
         samples_file, sep = ',', dtype = {'rep' : str})
 
     # Validate read file input with wildcard definitions
-    if not samples['cell_type'].str.match(r'[^-\.\/]+').all():
-        sys.exit(f'Invalid cell_type definition in {samples_file}.\n'
-            'Cell types must not contain the following characters: - . /')
-    if not samples['group'].str.match(r'[^-\.\/g]+').all():
-        sys.exit(f'Invalid group definition in {samples_file}.\n'
-            'Groups must not contain the following characters: - / . g')
+    if not samples['group'].str.match(r'[^\/\s.-]+').all():
+        sys.exit(f'Invalid group definition in {samples_file}.')
     if not samples['rep'].str.match(r'\d+').all():
-        sys.exit(f'Invalid replicate definition in {samples_file}.\n'
-            'Replicates must only contain integers.')
+        sys.exit(f'Invalid replicate definition in {samples_file}.')
     if not samples['read'].str.match(r'R[12]').all():
-        sys.exit(f'Invalid read definition in {samples_file}.\n'
-            'Reads must only be either "R1" or "R2".')
-
-    # Define single and sample names from definitions.
-    samples['single'] = (samples[['group', 'rep', 'read']]
-        .apply(lambda x: '-'.join(x), axis = 1))
-    samples['sample'] = (samples[['group', 'rep']]
-        .apply(lambda x: '-'.join(x), axis = 1))
-    # Ensure no duplicate names
-    if samples['single'].duplicated().all():
-        sys.exit(f'Duplicate sample name definitions in {samples_file}.\n')
-
-    samples = samples.set_index(
-        ['cell_type', 'group', 'sample', 'single'], drop = False)
+        sys.exit(f'Invalid read definition in {samples_file}.')
+    if not samples['type'].str.match(r'input|bound').all():
+        sys.exit(f'Invalid read definition in {samples_file}.')
 
     return samples
-
-
-def get_grouping(samples):
-
-    cell_types = {}
-    for cell in samples['cell_type']:
-        cell_types[cell] = list(samples.xs(cell, level=0)['sample'].unique())
-
-    groups = {}
-    for group in samples['group']:
-        groups[group] = list(samples.xs(group, level=1)['rep'].unique())
-    # Extract sample names
-    sample_list = list(samples['sample'].unique())
-
-    return sample_list, groups, cell_types
-
-
-def load_regions(regions_file):
-
-    regions = pd.read_table(
-        regions_file,
-        names=['chr', 'start', 'end', 'region'],
-        index_col='region',
-        dtype={'start': int, 'end': int})
-    regions['length'] = regions['end'] - regions['start']
-
-    # Validate read file input with wildcard definitions
-    if not regions.index.to_series().str.match(r'[^-\.\/]+').all():
-        sys.exit(f'Invalid region definition in {regions_file}.\n'
-            'Region names must not contain the following characters: - . /')
-
-    return regions
-
-
-def load_coords(files):
-    """ Read plot coordinates from plot coordinate and region BED file. """
-    coords = defaultdict(list)
-    for file in files:
-        if not file:
-            continue
-        with open(file) as fh:
-            for line in fh:
-                chr, start, end, region = line.strip().split()
-                coords[region].append(f'{chr}_{start}_{end}')
-    return coords
-
-def load_vcf_paths(phased_vcfs, samples):
-
-    vcfs = pd.read_table(
-        phased_vcfs, squeeze=True,
-        names=['cell_type', 'path'],
-        index_col='cell_type',sep = ',')
-
-    if not vcfs.index.str.match(r'[^-\.\/]+').all():
-        sys.exit(f'Invalid cell_type definition in {phased_vcfs}.\n'
-            'Cell types must not contain the following characters: - . /')
-
-    if len(vcfs.index.difference(samples.index.get_level_values('cell_type'))):
-        sys.exit(f'Differing cell types given in {phased_vcfs} compared '
-                 'to samples file.')
-
-    return vcfs
-
-
-def load_genomes(genomes_path):
-    genomes = pd.read_table(
-        genomes_path, squeeze=True,
-        names=['cell_type', 'path'],
-        index_col='cell_type', sep = ',')
-    return genomes
-
-
-def get_allele_groupings(samples):
-
-    allele_groups = defaultdict(list)
-    allele_samples = []
-    for sample in samples:
-        sample = sample.split('-')
-        group = sample[0]
-        rep = sample[1]
-        allele_samples.extend([f'{group}_g1-{rep}', f'{group}_g2-{rep}'])
-        allele_groups[f'{group}_g1'].append(rep)
-        allele_groups[f'{group}_g2'].append(rep)
-
-    return allele_groups, allele_samples
